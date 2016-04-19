4c5011d9c16c1aad58e5c450c964d23c8a714727
Richard Hosking <r.hosking@auckland.ac.nz>
2016-04-19 13:56:42 +1200

    shrunk padding on lifecycle headers

--- sites/all/themes/materializecss/templates/views-view-unformatted--research-lifecycle--block-1.tpl.php
+++ sites/all/themes/materializecss/templates/views-view-unformatted--research-lifecycle--block-1.tpl.php
@@ -17,7 +17,7 @@
 foreach ($terms as $t) {
   $machine_name = str_replace(' &', '', $t->name);
   $machine_name = str_replace(' ', '_', $machine_name);
-  echo "<span class='lifecycle_selector $machine_name' style='display:inline-block;text-align:center;padding:20px 0px;color:white;font-family:Roboto,sans-serif;font-size:20px;font-weight:normal;width:20%;background-color:{$t->field_color['und'][0]['rgb']}'>{$t->name}</span>";
+  echo "<span class='lifecycle_selector $machine_name' style='display:inline-block;text-align:center;padding:20px;color:white;font-family:Roboto,sans-serif;font-size:20px;font-weight:normal;width:20%;background-color:{$t->field_color['und'][0]['rgb']}'>{$t->name}</span>";
 }
 ?>
 <?php foreach ($rows as $id => $row): ?>
