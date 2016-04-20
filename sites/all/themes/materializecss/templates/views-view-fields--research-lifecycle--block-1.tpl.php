be6b153df881a78a113ceffcfa5801b94959a4c9
Nick Young <nick.young@auckland.ac.nz>
2016-04-20 17:31:18 +1200

    zebra stripe section on front page, min height on lifecycle details

--- sites/all/themes/materializecss/templates/views-view-fields--research-lifecycle--block-1.tpl.php
+++ sites/all/themes/materializecss/templates/views-view-fields--research-lifecycle--block-1.tpl.php
@@ -32,13 +32,7 @@
  }
 ?>
 
-<div class="col s12 m12 research_lifecycle_stage <?php echo $machine_name ?>"
-  <?php
-  if ($machine_name != "Plan_Design"){
-    echo 'style="display:none;padding:0px"';
-  } else {
-    echo 'style="padding:0px"';
-  } ?>>
+<div class="col s12 m12 research_lifecycle_stage <?php echo $machine_name ?>">
     <div class="card">
       <div class="background" style="background-color:<?php echo $fields['field_color']->content ?>;opacity:.04;position:absolute;top:0;left:0;width:100%;height:100%;z-index: 1;pointer-events: none;"></div>
         <div class="card-content">
