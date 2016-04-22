6ad4171c912950ad46bbf8c687db9682bdae5723
Richard Hosking <r.hosking@auckland.ac.nz>
2016-04-22 16:03:52 +1200

    added title to edu

--- sites/all/themes/materializecss/templates/views-view-unformatted--education--block.tpl.php
+++ sites/all/themes/materializecss/templates/views-view-unformatted--education--block.tpl.php
@@ -10,7 +10,7 @@
 
 <div class="row" style="margin-bottom:0px;">
     <div class="col s12">
-      <h2 style="color:rgb(124, 179, 66)">Education, Training and Events</h2>
+      <h4 style="color:rgb(124, 179, 66)">Education, Training and Events</h2>
         <!--<div class="card">
             <div class="card-image white-text" style="max-height:60px">
                 <img typeof="foaf:Image" src="https://researchit.cer.auckland.ac.nz/sites/default/files/light-green-darken1.jpg" alt="" height="60" width="400">
