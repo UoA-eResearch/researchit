5d76966f270695f19ea3b7862f45df39e6ed2442
Richard Hosking <r.hosking@auckland.ac.nz>
2016-04-27 16:51:25 +1200

    cleaned up project page

--- sites/all/modules/researchit_user/templates/researchit-user-project.tpl.php
+++ sites/all/modules/researchit_user/templates/researchit-user-project.tpl.php
@@ -1,11 +1,12 @@
 <div class="row">
-
-</div>
-
-<div class="row">
   <div class="col s8">
     <h4><?php echo $pw->project->name ?></h4>
     <div class='editable' id='project_description' data-type="wysihtml5" data-pk="Project" data-name="Description" data-title="Description"><?php echo strip_tags($pw->project->description) ?></div>
+
+    <!-- Left Column content goes here -->
+
+
+
   </div>
   <div class="col s4" style="padding-top: 15px">
 
