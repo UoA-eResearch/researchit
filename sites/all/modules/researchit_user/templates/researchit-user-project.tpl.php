f862c412c4abffaee9a257c827510ac2ecb3070c
Richard Hosking <r.hosking@auckland.ac.nz>
2016-04-27 15:36:45 +1200

    updated page structure for research projects

--- sites/all/modules/researchit_user/templates/researchit-user-project.tpl.php
+++ sites/all/modules/researchit_user/templates/researchit-user-project.tpl.php
@@ -7,9 +7,29 @@
     <h4><?php echo $pw->project->name ?></h4>
     <div class='editable' id='project_description' data-type="wysihtml5" data-pk="Project" data-name="Description" data-title="Description"><?php echo strip_tags($pw->project->description) ?></div>
   </div>
-  <div class="col s4">
-    <h5>Researchers on project</h4>
-    <ul class="collection">
+  <div class="col s4" style="padding-top: 15px">
+
+    <table class="project_main">
+      <tr>
+        <td>Code:</td>
+        <td><?php echo $pw->project->projectCode ?></td>
+      </tr>
+      <tr>
+        <td>Status:</td>
+        <td><?php echo $pw->project->statusName ?></td>
+      </tr>
+      <tr>
+        <td>First Day:</td>
+        <td><?php echo $pw->project->startDate ?></td>
+      </tr>
+      <tr>
+        <td>Last Day:</td>
+        <td><?php echo $pw->project->endDate ?></td>
+      </tr>
+    </table>
+
+    <ul class="collection" style="border:0px">
+      <li class="collection-header"><h5>Researchers on project</h5></li>
       <?php foreach ($pw->rpLinks as $r): ?>
       <li class="collection-item avatar">
         <i class="material-icons circle">folder</i>
@@ -21,22 +41,3 @@
     </ul>
   </div>
 </div>
-
-<table class="project_main">
-  <tr>
-    <td>Code:</td>
-    <td><?php echo $pw->project->projectCode ?></td>
-  </tr>
-  <tr>
-    <td>Status:</td>
-    <td><?php echo $pw->project->statusName ?></td>
-  </tr>
-  <tr>
-    <td>First Day:</td>
-    <td><?php echo $pw->project->startDate ?></td>
-  </tr>
-  <tr>
-    <td>Last Day:</td>
-    <td><?php echo $pw->project->endDate ?></td>
-  </tr>
-</table>
