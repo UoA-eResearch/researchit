1bbc07ad5916761f24e7514652e3838387f2bcb9
Apache <apache@cerhosprd04.cer.auckland.ac.nz>
2016-04-28 17:10:08 +1200

    add template for project dashboard

---
+++ sites/all/modules/researchit_user/templates/researchit-user-project-dashboard.tpl.php
@@ -0,0 +1,24 @@
+<h2>My projects</h2>
+<div class="row">
+<?php foreach ($projectdb_info->projects as $p):
+        if (in_array($p->statusId, array(1,2,6))) {
+          $color = '#43a047';
+        } else if (in_array($p->statusId, array(4,10))) {
+          $color = '#e53935';
+        } else {
+          $color = '#1e88e5';
+        }
+?>
+  <div class="col s12 m4">
+    <a href='/projects/<?php echo $p->projectCode ?>'>
+      <div class="card" style="color:<?php echo $color ?>;min-height:250px">
+        <div class="card-content">
+          <span class="card-title"><?php echo $p->name; ?></span>
+          <p><?php echo truncate_utf8($p->description, 100, TRUE, TRUE); ?></p>
+          <p style='color:black'><?php echo $p->projectCode ?></p>
+        </div>
+      </div>
+    </a>
+  </div>
+<?php endforeach; ?>
+</div>
