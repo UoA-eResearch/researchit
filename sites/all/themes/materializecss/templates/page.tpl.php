f5382258f67c7585c9539a1b23b16cba20582804
Apache <apache@cerhosprd04.cer.auckland.ac.nz>
2016-08-04 17:07:42 +1200

    display cer projects in the same fashion as NeSI projects

--- sites/all/themes/materializecss/templates/page.tpl.php
+++ sites/all/themes/materializecss/templates/page.tpl.php
@@ -226,9 +226,9 @@
               if (!empty($projects)) {
                 foreach ($projects as $i => $p) {
                   $desc = truncate_utf8($p->description, 100, TRUE, TRUE);
-                  if (in_array($p->statusId, array(1,2,6))) {
+                  if (in_array($p->statusName, array('Open', 'Reopened', 'Open-Not Public'))) {
                     $color = '#43a047';
-                  } else if (in_array($p->statusId, array(4,10))) {
+                  } else if (in_array($p->statusId, array('Closed', 'Rejected'))) {
                     $color = '#e53935';
                   } else {
                     $color = '#1e88e5';
