a47c0f6dd0d027f215c5e8064993cc5965735fcd
Apache <apache@cerhosprd04.cer.auckland.ac.nz>
2016-08-08 17:07:00 +1200

    Responsive tweaks

--- sites/all/themes/materializecss/templates/views-view-unformatted--category-taxonomy--category-accordion.tpl.php
+++ sites/all/themes/materializecss/templates/views-view-unformatted--category-taxonomy--category-accordion.tpl.php
@@ -7,7 +7,7 @@
     $machine_name = str_replace(' &', '', $t->name);
     $machine_name = str_replace(' ', '_', $machine_name);
     $glyph = $t->field_glypthcategory['und'][0]['value'];
-    echo "<div class='category_selector $machine_name' style='text-align:center;padding:15px;margin-bottom:5px;color:white;font-family:Roboto,sans-serif;font-size:20px;font-weight:normal;'><i class='material-icons circle' style='float:left'>$glyph</i><i class='material-icons arrow $machine_name'>play_arrow</i>{$t->name}</div>";
+    echo "<div class='category_selector $machine_name' style='text-align:center;padding:15px;margin-bottom:5px;color:white;'><i class='material-icons circle' style='float:left'>$glyph</i><i class='material-icons arrow $machine_name'>play_arrow</i><p class='scale-text'>{$t->name}</p></div>";
   }
   echo "</div><div class='col s9'>";
   foreach ($rows as $id => $row) {
