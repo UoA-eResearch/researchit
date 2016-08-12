b6494634d0718c58ae2f8c4c3d7274d3f2ef7fbd
Jamie Diprose <jamie.diprose@gmail.com>
2016-08-12 15:43:48 +1200

    Updated structure and content of sidenav.

--- sites/all/themes/materializecss/templates/page.tpl.php
+++ sites/all/themes/materializecss/templates/page.tpl.php
@@ -1,116 +1,69 @@
 <?php
-/**
- * @file
- * Materialize theme implementation to display a single Drupal page.
- *
- * The doctype, html, head and body tags are not in this template. Instead they
- * can be found in the html.tpl.php template in this directory.
- *
- * Available variables:
- *
- * General utility variables:
- * - $base_path: The base URL path of the Drupal installation. At the very
- *   least, this will always default to /.
- * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
- * - $is_front: TRUE if the current page is the front page.
- * - $logged_in: TRUE if the user is registered and signed in.
- * - $is_admin: TRUE if the user has permission to access administration pages.
- *
- * Site identity:
- * - $front_page: The URL of the front page. Use this instead of $base_path,
- *   when linking to the front page. This includes the language domain or
- *   prefix.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $site_name: The name of the site, empty when display has been disabled
- *   in theme settings.
- * - $site_slogan: The slogan of the site, empty when display has been disabled
- *   in theme settings.
- *
- * Navigation:
- * - $main_menu (array): An array containing the Main menu links for the
- *   site, if they have been configured.
- * - $secondary_menu (array): An array containing the Secondary menu links for
- *   the site, if they have been configured.
- * - $breadcrumb: The breadcrumb trail for the current page.
- *
- * Page content (in order of occurrence in the default page.tpl.php):
- * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front of the main title tag that
- *   appears in the template.
- * - $title: The page title, for use in the actual HTML content.
- * - $title_suffix (array): An array containing additional output populated by
- *   modules, intended to be displayed after the main title tag that appears in
- *   the template.
- * - $messages: HTML for status and error messages. Should be displayed
- *   prominently.
- * - $tabs (array): Tabs linking to any sub-pages beneath the current page
- *   (e.g., the view and edit tabs when displaying a node).
- * - $action_links (array): Actions local to the page, such as 'Add menu' on the
- *   menu administration interface.
- * - $feed_icons: A string of all feed icons for the current page.
- * - $node: The node object, if there is an automatically-loaded node
- *   associated with the page, and the node ID is the second argument
- *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
- *   comment/reply/12345).
- *
- * Regions:
- * - $page['help']: Dynamic help text, mostly for admin pages.
- * - $page['highlighted']: Items for the highlighted content region.
- * - $page['content']: The main content of the current page.
- * - $page['sidebar_first']: Items for the first sidebar.
- * - $page['sidebar_second']: Items for the second sidebar.
- * - $page['header']: Items for the header region.
- * - $page['footer']: Items for the footer region.
- *
- * @see bootstrap_preprocess_page()
- * @see template_preprocess()
- * @see template_preprocess_page()
- * @see bootstrap_process_page()
- * @see template_process()
- * @see html.tpl.php
- *
- * @ingroup themeable
- */
-
-foreach ($primary_nav as &$l) {
-    if (!empty($l['#attributes'])) {
-        $l['#attributes']['class'] = array('test');
-    }
-}
+$name = '';
+$email = '';
+$pictureUrl = 'https://projects.nesi.org.nz/sites/default/files/nesi_avatar.png';
+$projects = array();
 
 if (user_is_logged_in()) {
     global $user;
-    $text = 'Logged in as ' . $user->name;
-    $href = '/projects';
-    $class = array('username', 'logged_in');
+    $projects = researchit_user_get_projects();
+    $userData = $user->data['projectdb_info'];
 
-} else {
-    $text = 'Login';
-    $href = '/Shibboleth.sso/Login';
-    $class = array('username', 'not_logged_in');
+    if($userData && $userData->fullName)
+    {
+        $name = $userData->fullName;
+    }
+
+    if($userData && $userData->email)
+    {
+        $email = $userData->email;
+    }
+
+    if($userData && $userData->pictureUrl)
+    {
+        $pictureUrl = $userData->pictureUrl;
+    }
 }
-$primary_nav[1000] = array('#theme' => 'menu_link__main_menu', '#title' => $text, '#href' => $href, '#below' => '', '#attributes' => array('class' => $class));
-$primary_nav[1001] = array('#theme' => 'menu_link__main_menu', '#title' => 'Search', '#href' => 'search', '#below' => '', '#attributes' => array('class' => 'search_button'));
 ?>
-
-
 
 <nav class="top-nav bg-light-grey" id="nav">
     <ul id='slide-out' class='side-nav'>
         <li><div class="userView">
                 <img class="background" src="/sites/default/files/abstract-background.jpg">
-                <a href="#!user"><img class="circle" src="https://projects.nesi.org.nz/sites/default/files/nesi_avatar.png"></a>
-                <a href="#!name"><span class="white-text name">John Doe</span></a>
-                <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+                <?php echo "<a href='/'><img src='{$pictureUrl}' class='circle'></a>"; ?>
+                <?php echo "<a href='/'><span class='white-text name'>$name</span></a>" ?>
+                <?php echo "<a href='/'><span class='white-text email'>$email</span></a>" ?>
             </div></li>
-        <li><a href="/"><i class="material-icons">home</i>Home</a></li>
-        <li><a href="#!">Second Link</a></li>
-        <li><a href="/"><i class="material-icons">library_books</i>Projects</a></li>
-        <li><a class="subheader">Subheader</a></li>
-<!--        <li><div class="divider"></div></li>-->
+        <li class="bold"><a href="/" class="waves-effect waves-teal"><i class="material-icons">home</i>Home</a></li>
+        <li class="bold"><a href="/projects" class="waves-effect waves-teal"><i class="material-icons">library_books</i>Projects</a></li>
+        <li class="bold"><a href="/#category_accordion" class="waves-effect waves-teal"><i class="material-icons">settings</i>Services</a></li>
+        <li class="bold"><a href="/#education" class="waves-effect waves-teal"><i class="material-icons">school</i>Education</a></li>
+        <li><div class="divider"></div></li>
+        <li class="no-padding">
+            <ul class="collapsible collapsible-accordion">
+                <li class="bold">
+                    <a class="collapsible-header waves-effect waves-teal">My Projects<i class="material-icons right" style="margin-right: 5px;">arrow_drop_down</i></a>
+                    <div class="collapsible-body" style="">
+                        <ul>
+                            <?php
+                                if (!empty($projects)) {
+                                    foreach ($projects as $i => $p) {
+                                        $pName = $p->name;
 
-<!--        <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>-->
+                                        if (strlen($pName) > 30)
+                                        {
+                                            $pName = substr($pName, 0, 30) . '...';
+                                        }
+
+                                        echo "<li><a href='/projects/{$p->projectCode}'>{$pName}</a></li>";
+                                    }
+                                }
+                            ?>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </li>
     </ul>
 
     <div class="nav-wrapper">
@@ -118,32 +71,17 @@ $primary_nav[1001] = array('#theme' => 'menu_link__main_menu', '#title' => 'Sear
         <a href="#" class="nav-left brand-logo">The Research Hub</a>
 
         <a class="waves-effect waves-circle btn-floating nav-user-btn white right dropdown-button" data-activates='user-details' data-beloworigin="true" data-constrainwidth="false">
-            <?php
-                if (user_is_logged_in()) {
-                    $pictureUrl = $user->data['projectdb_info']->pictureUrl;
-
-                    if($pictureUrl)
-                        echo "<img src='{$pictureUrl}' class='circle'>";
-                    else
-                        echo "<img src='https://projects.nesi.org.nz/sites/default/files/nesi_avatar.png' class='circle'/>";
-                } else {
-                    echo "<img src='https://projects.nesi.org.nz/sites/default/files/nesi_avatar.png' class='circle'/>";
-                };
-            ?>
+            <?php echo "<img src='{$pictureUrl}' class='circle'>"; ?>
         </a>
 
         <ul id='user-details' class='dropdown-content collection' style="width: 300px;"> 
             <?php
                 if (user_is_logged_in()) {
-                    $pictureUrl = $user->data['projectdb_info']->pictureUrl;
-
-                    if ($pictureUrl) {
-                        echo "<li class='collection-item avatar'><img src='{$pictureUrl}' class='circle'><span class='title'>$user->name</span></li>";
-                    }
+                    echo "<li class='collection-item avatar'><img src='{$pictureUrl}' class='circle'><span class='title'>$name</span></li>";
                 }
                 else
                 {
-                    echo "<li class='collection-item avatar'><img src='https://projects.nesi.org.nz/sites/default/files/nesi_avatar.png' class='circle'/><span class='title'>Not logged in</span></li><li class='collection-item' href='Shibboleth.sso/Login'>Login</li>";
+                    echo "<li class='collection-item avatar'><img src='{$pictureUrl}' class='circle'/><span class='title'>Not logged in</span></li><li class='collection-item' href='Shibboleth.sso/Login'>Login</li>";
                 }
             ?>
          </ul>
